ad4044e3b4db2d95d94087c99c42de704bc6af56
mizanur87 <mirzarazmizan87@gmail.com>
2026-03-26 03:49:21 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Countries from "./component/Countries";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <Countries></Countries>
+      <div className="container mx-auto text-center text-gray-900"></div>
+    </>
+  );
+}
+
+export default App;
